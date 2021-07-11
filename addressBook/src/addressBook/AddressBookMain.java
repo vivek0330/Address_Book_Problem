093a7227ad976ddf8fec28455e0b26322ee8a2af
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-11 20:08:22 +0530

    edit person details

--- addressBook/src/addressBook/AddressBookMain.java
+++ addressBook/src/addressBook/AddressBookMain.java
@@ -1,5 +1,8 @@
 package addressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Person {
 
 	// Declare variables
@@ -77,10 +80,73 @@ class Person {
 		this.mobNumber = mobNumber;
 	}
 
+	@Override
+	public String toString() {
+		return '\n' + "Contact is" + '\n' + "First Name :: " + firstName + '\n' + "Last  Name :: " + lastName + '\n'
+				+ "City : " + city + '\n' + "State :: " + state + "zip :: " + pinCode + '\n' + "Phone Number :: "
+				+ mobNumber + '\n' + "Email Id :: " + email + '\n';
+
+	}
+
+	public void addDetails() {
+
+		// taking value from user
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Add person Deatils");
+		System.out.print("Enter First Name :: ");
+		this.firstName = sc.nextLine();
+		System.out.print("Enter Last Name :: ");
+		this.lastName = sc.nextLine();
+		System.out.print("Enter City :: ");
+		this.city = sc.nextLine();
+		System.out.print("Enter State :: ");
+		this.state = sc.nextLine();
+		System.out.print("Enter pinCode :: ");
+		this.pinCode = sc.nextLong();
+		System.out.print("Enter mobNumber :: ");
+		this.mobNumber = sc.nextLong();
+		System.out.print("Enter Email ID :: ");
+		this.email = sc.next();
+	}
+
+	public void Display_All(ArrayList<Person> arr) {
+		boolean is_Empty = arr.isEmpty();
+		if (is_Empty == true)
+			System.out.println("Array List is Empty");
+		else
+			System.out.println(arr);
+	}
+
 }
 
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book Program");
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Person> list = new ArrayList<Person>();
+		int option;
+		do {
+			Person personDetails = new Person();
+			System.out.println("Enter your option");
+			System.out.println("1 Add contact  ");
+			System.out.println("2 Display all contact ");
+
+			option = sc.nextInt();
+
+			switch (option) {
+			case 1:
+				personDetails.addDetails();
+				list.add(personDetails);
+				break;
+			case 2:
+				personDetails.Display_All(list);
+				break;
+			default:
+				System.out.println("Enter valid option");
+				break;
+			}
+			System.out.println("Do you want to continue (1 Continue \n 0 Exit)");
+			option = sc.nextInt();
+		} while (option != 0);
 	}
 }
